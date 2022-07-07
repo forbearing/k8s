88bf5bc14b8f880770854c00f1922ef8172397e4
hybfkuf <fredjoseph863@gmail.com>
2022-07-07 14:16:26 +0800

    Change logrus to klog

--- delete_f.go
+++ delete_f.go
@@ -29,13 +29,13 @@ import (
 	"github.com/forbearing/k8s/serviceaccount"
 	"github.com/forbearing/k8s/statefulset"
 	"github.com/forbearing/k8s/storageclass"
-	"github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	networking "k8s.io/api/networking/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	storagev1 "k8s.io/api/storage/v1"
+	"k8s.io/klog"
 )
 
 // DeleteF like "kubectl delete -f filename"
@@ -57,7 +57,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 		}
 		object, err := Decode(k8sResource)
 		if err != nil {
-			logrus.Errorf("Decode error: %v\n", err)
+			klog.V(4).Info(err)
 			continue
 		}
 		switch object.(type) {
@@ -66,207 +66,184 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 			if err != nil {
 				return err
 			}
-			ns, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete namespace %q failed: %v\n", ns.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.Service:
 			handler, err := service.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			svc, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete service %q failed: %v\n", svc.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.ConfigMap:
 			handler, err := configmap.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			cm, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete configmap %q failed: %v\n", cm.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.Secret:
 			handler, err := secret.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			sec, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete secret %q failed: %v\n", sec.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.ServiceAccount:
 			handler, err := serviceaccount.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			sa, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete serviceaccount %q failed: %v\n", sa.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.Pod:
 			handler, err := pod.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			p, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete pod %q failed: %v\n", p.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.PersistentVolume:
 			handler, err := persistentvolume.New(ctx, kubeconfig)
 			if err != nil {
 				return err
 			}
-			pv, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete persistentvolume %q failed: %v\n", pv.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.PersistentVolumeClaim:
 			handler, err := persistentvolumeclaim.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			pvc, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete persistentvolumeclaim %q failed: %v\n", pvc.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *appsv1.Deployment:
 			handler, err := deployment.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			deploy, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete deployment %q failed: %v\n", deploy.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *appsv1.StatefulSet:
 			handler, err := statefulset.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			sts, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete statefulset %q failed: %v\n", sts.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *appsv1.DaemonSet:
 			handler, err := daemonset.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			ds, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete daemonset %q failed: %v\n", ds.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *networking.Ingress:
 			handler, err := ingress.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			ing, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete ingress %q failed: %v\n", ing.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *networking.IngressClass:
 			handler, err := ingressclass.New(ctx, kubeconfig)
 			if err != nil {
 				return err
 			}
-			ingc, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete ingressclass %q failed: %v\n", ingc.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *networking.NetworkPolicy:
 			handler, err := networkpolicy.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			netpol, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete networkpolicy %q failed: %v\n", netpol.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *batchv1.Job:
 			handler, err := job.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			job, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete job %q failed: %v\n", job.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *batchv1.CronJob:
 			handler, err := cronjob.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			cj, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete cronjob %q failed: %v\n", cj.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *rbacv1.Role:
 			handler, err := role.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			r, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete role %q failed: %v\n", r.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *rbacv1.RoleBinding:
 			handler, err := rolebinding.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			rb, _ := handler.CreateFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete rolebinding %q failed", rb.Name)
+				klog.V(4).Info(err)
 			}
 		case *rbacv1.ClusterRole:
 			handler, err := clusterrole.New(ctx, kubeconfig)
 			if err != nil {
 				return err
 			}
-			cr, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete clusterrole %q failed: %v\n", cr.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *rbacv1.ClusterRoleBinding:
 			handler, err := clusterrolebinding.New(ctx, kubeconfig)
 			if err != nil {
 				return err
 			}
-			crb, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete clusterrolebinding %q failed: %v\n", crb.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *appsv1.ReplicaSet:
 			handler, err := replicaset.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			rs, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete replicaset %q failed: %v\n", rs.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.ReplicationController:
 			handler, err := replicationcontroller.New(ctx, "", kubeconfig)
 			if err != nil {
 				return err
 			}
-			rc, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete replicationcontroller %q failed: %v\n", rc.Name, err)
+				klog.V(4).Info(err)
 			}
 		case *storagev1.StorageClass:
 			handler, err := storageclass.New(ctx, kubeconfig)
 			if err != nil {
 				return err
 			}
-			sc, _ := handler.GetFromBytes(k8sResource)
 			if err := handler.DeleteFromBytes(k8sResource); err != nil {
-				logrus.Errorf("delete storageclass %q failed: %v\n", sc.Name, err)
+				klog.V(4).Info(err)
 			}
 		default:
 		}
--- go.mod
+++ go.mod
@@ -8,15 +8,13 @@ require (
 	k8s.io/api v0.24.2
 	k8s.io/apimachinery v0.24.2
 	k8s.io/client-go v0.24.2
+	k8s.io/klog v1.0.0
 	k8s.io/metrics v0.24.2
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9
 )
 
 require (
-	github.com/PuerkitoBio/purell v1.1.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/emicklei/go-restful v2.15.0+incompatible // indirect
 	github.com/emicklei/go-restful/v3 v3.8.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
