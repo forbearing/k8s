d48261f94d6376022b8b34a70380f5ec88ba5fa2
hybfkuf <fredjoseph863@gmail.com>
2022-07-23 19:57:21 +0800

    More easy create/update/apply/delete/get pod resources

--- pod/delete.go
+++ pod/delete.go
@@ -2,13 +2,53 @@ package pod
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
-// DeleteFromBytes delete pod from bytes.
+// Delete deletes pod from type string, []byte, *corev1.Pod,
+// corev1.Pod, runtime.Object or map[string]interface{}.
+
+// If passed parameter type is string, it will simply call DeleteByName instead of DeleteFromFile.
+// You should always explicitly call DeleteFromFile to delete a pod from file path.
+func (h *Handler) Delete(obj interface{}) error {
+	switch val := obj.(type) {
+	case string:
+		return h.DeleteByName(val)
+	case []byte:
+		return h.DeleteFromBytes(val)
+	case *corev1.Pod:
+		return h.DeleteFromObject(val)
+	case corev1.Pod:
+		return h.DeleteFromObject(&val)
+	case runtime.Object:
+		return h.DeleteFromObject(val)
+	case map[string]interface{}:
+		return h.DeleteFromUnstructured(val)
+	default:
+		return ERR_TYPE_DELETE
+	}
+}
+
+// DeleteByName deletes pod by name.
+func (h *Handler) DeleteByName(name string) error {
+	return h.clientset.CoreV1().Pods(h.namespace).Delete(h.ctx, name, h.Options.DeleteOptions)
+}
+
+// DeleteFromFile deletes pod from yaml file.
+func (h *Handler) DeleteFromFile(filename string) error {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	return h.DeleteFromBytes(data)
+}
+
+// DeleteFromBytes deletes pod from bytes.
 func (h *Handler) DeleteFromBytes(data []byte) error {
 	podJson, err := yaml.ToJSON(data)
 	if err != nil {
@@ -21,31 +61,36 @@ func (h *Handler) DeleteFromBytes(data []byte) error {
 		return err
 	}
 
+	return h.deletePod(pod)
+}
+
+// DeleteFromObject deletes pod from runtime.Object.
+func (h *Handler) DeleteFromObject(obj runtime.Object) error {
+	pod, ok := obj.(*corev1.Pod)
+	if !ok {
+		return fmt.Errorf("object is not *corev1.Pod")
+	}
+	return h.deletePod(pod)
+}
+
+// DeleteFromUnstructured deletes pod from map[string]interface{}.
+func (h *Handler) DeleteFromUnstructured(u map[string]interface{}) error {
+	pod := &corev1.Pod{}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u, pod)
+	if err != nil {
+		return err
+	}
+
+	return h.deletePod(pod)
+}
+
+// deletePod
+func (h *Handler) deletePod(pod *corev1.Pod) error {
 	var namespace string
 	if len(pod.Namespace) != 0 {
 		namespace = pod.Namespace
 	} else {
 		namespace = h.namespace
 	}
-
-	return h.WithNamespace(namespace).DeleteByName(pod.Name)
-}
-
-// DeleteFromFile delete pod from yaml file.
-func (h *Handler) DeleteFromFile(filename string) error {
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return err
-	}
-	return h.DeleteFromBytes(data)
-}
-
-// DeleteByName delete pod by name.
-func (h *Handler) DeleteByName(name string) error {
-	return h.clientset.CoreV1().Pods(h.namespace).Delete(h.ctx, name, h.Options.DeleteOptions)
-}
-
-// Delete delete pod by name, alias to "DeleteByName".
-func (h *Handler) Delete(name string) error {
-	return h.DeleteByName(name)
+	return h.clientset.CoreV1().Pods(namespace).Delete(h.ctx, pod.Name, h.Options.DeleteOptions)
 }
