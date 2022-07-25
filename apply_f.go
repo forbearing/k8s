58f8f45e71c24e4aa29768920dc032410b3a364a
hybfkuf <fredjoseph863@gmail.com>
2022-07-25 10:41:13 +0800

    Change logrus to klog

--- apply_f.go
+++ apply_f.go
@@ -29,7 +29,6 @@ import (
 	"github.com/forbearing/k8s/serviceaccount"
 	"github.com/forbearing/k8s/statefulset"
 	"github.com/forbearing/k8s/storageclass"
-	log "github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -38,6 +37,7 @@ import (
 	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/klog"
 )
 
 // ApplyF like "kubectl apply -f filename.yaml".
@@ -61,7 +61,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 		}
 		object, err := Decode(k8sResource)
 		if err != nil {
-			log.Errorf("Decode error: %v", err)
+			klog.V(4).Info("Decode error: %v", err)
 			continue
 		}
 		switch object.(type) {
@@ -71,7 +71,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if ns, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply namespace %q failed: %v\n", ns.Name, err)
+				klog.V(4).Info("apply namespace %q failed: %v\n", ns.Name, err)
 			}
 		case *corev1.Service:
 			handler, err := service.New(ctx, kubeconfig, "")
@@ -79,7 +79,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if svc, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply service %q failed: %v\n", svc.Name, err)
+				klog.V(4).Info("apply service %q failed: %v\n", svc.Name, err)
 			}
 		case *corev1.ConfigMap:
 			handler, err := configmap.New(ctx, kubeconfig, "")
@@ -87,7 +87,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if cm, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply configmap %q failed: %v\n", cm.Name, err)
+				klog.V(4).Info("apply configmap %q failed: %v\n", cm.Name, err)
 			}
 		case *corev1.Secret:
 			handler, err := secret.New(ctx, kubeconfig, "")
@@ -95,7 +95,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if sec, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply secret %q failed: %v\n", sec.Name, err)
+				klog.V(4).Info("apply secret %q failed: %v\n", sec.Name, err)
 			}
 		case *corev1.ServiceAccount:
 			handler, err := serviceaccount.New(ctx, kubeconfig, "")
@@ -103,7 +103,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if sa, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply serviceaccount %q failed: %v\n", sa.Name, err)
+				klog.V(4).Info("apply serviceaccount %q failed: %v\n", sa.Name, err)
 			}
 		case *corev1.Pod:
 			handler, err := pod.New(ctx, kubeconfig, "")
@@ -111,7 +111,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if p, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply pod %q failed: %v\n", p.Name, err)
+				klog.V(4).Info("apply pod %q failed: %v\n", p.Name, err)
 			}
 		case *corev1.PersistentVolume:
 			handler, err := persistentvolume.New(ctx, kubeconfig)
@@ -119,7 +119,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if pv, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply persistentvolume %q failed: %v\n", pv.Name, err)
+				klog.V(4).Info("apply persistentvolume %q failed: %v\n", pv.Name, err)
 			}
 		case *corev1.PersistentVolumeClaim:
 			handler, err := persistentvolumeclaim.New(ctx, kubeconfig, "")
@@ -127,7 +127,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if pvc, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply persistentvolumeclaim %q failed: %v\n", pvc.Name, err)
+				klog.V(4).Info("apply persistentvolumeclaim %q failed: %v\n", pvc.Name, err)
 			}
 		case *appsv1.Deployment:
 			handler, err := deployment.New(ctx, kubeconfig, "")
@@ -135,7 +135,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if deploy, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply deployment %q failed: %v\n", deploy.Name, err)
+				klog.V(4).Info("apply deployment %q failed: %v\n", deploy.Name, err)
 			}
 		case *appsv1.StatefulSet:
 			handler, err := statefulset.New(ctx, kubeconfig, "")
@@ -143,7 +143,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if sts, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply statefulset %q failed: %v\n", sts.Name, err)
+				klog.V(4).Info("apply statefulset %q failed: %v\n", sts.Name, err)
 			}
 		case *appsv1.DaemonSet:
 			handler, err := daemonset.New(ctx, kubeconfig, "")
@@ -151,7 +151,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if ds, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply daemonset %q failed: %v\n", ds.Name, err)
+				klog.V(4).Info("apply daemonset %q failed: %v\n", ds.Name, err)
 			}
 		case *networking.Ingress:
 			handler, err := ingress.New(ctx, kubeconfig, "")
@@ -159,7 +159,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if ing, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply ingress %q failed: %v\n", ing.Name, err)
+				klog.V(4).Info("apply ingress %q failed: %v\n", ing.Name, err)
 			}
 		case *networking.IngressClass:
 			handler, err := ingressclass.New(ctx, kubeconfig)
@@ -167,7 +167,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if ingc, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply ingressclass %q failed: %v\n", ingc.Name, err)
+				klog.V(4).Info("apply ingressclass %q failed: %v\n", ingc.Name, err)
 			}
 		case *networking.NetworkPolicy:
 			handler, err := networkpolicy.New(ctx, kubeconfig, "")
@@ -175,7 +175,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if netpol, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply networkpolicy %q failed: %v\n", netpol.Name, err)
+				klog.V(4).Info("apply networkpolicy %q failed: %v\n", netpol.Name, err)
 			}
 		case *batchv1.Job:
 			handler, err := job.New(ctx, kubeconfig, "")
@@ -183,7 +183,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if j, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply job %q failed: %v\n", j.Name, err)
+				klog.V(4).Info("apply job %q failed: %v\n", j.Name, err)
 			}
 		case *batchv1.CronJob:
 			handler, err := cronjob.New(ctx, kubeconfig, "")
@@ -191,7 +191,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if cj, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply cronjob %q failed: %v\n", cj.Name, err)
+				klog.V(4).Info("apply cronjob %q failed: %v\n", cj.Name, err)
 			}
 		case *rbacv1.Role:
 			handler, err := role.New(ctx, kubeconfig, "")
@@ -199,7 +199,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if r, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply role %q failed: %v\n", r.Name, err)
+				klog.V(4).Info("apply role %q failed: %v\n", r.Name, err)
 			}
 		case *rbacv1.RoleBinding:
 			handler, err := rolebinding.New(ctx, kubeconfig, "")
@@ -207,7 +207,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if rb, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply rolebinding %q failed: %v\n", rb.Name, err)
+				klog.V(4).Info("apply rolebinding %q failed: %v\n", rb.Name, err)
 			}
 		case *rbacv1.ClusterRole:
 			handler, err := clusterrole.New(ctx, kubeconfig)
@@ -215,7 +215,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if cr, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply clusterrole %q failed: %v\n", cr.Name, err)
+				klog.V(4).Info("apply clusterrole %q failed: %v\n", cr.Name, err)
 			}
 		case *rbacv1.ClusterRoleBinding:
 			handler, err := clusterrolebinding.New(ctx, kubeconfig)
@@ -223,7 +223,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if crb, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply clusterrolebinding %q failed: %v\n", crb.Name, err)
+				klog.V(4).Info("apply clusterrolebinding %q failed: %v\n", crb.Name, err)
 			}
 		case *corev1.ReplicationController:
 			handler, err := replicationcontroller.New(ctx, kubeconfig, "")
@@ -231,7 +231,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if rc, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply replicationcontroller %q failed: %v\n", rc.Name, err)
+				klog.V(4).Info("apply replicationcontroller %q failed: %v\n", rc.Name, err)
 			}
 		case *appsv1.ReplicaSet:
 			handler, err := replicaset.New(ctx, kubeconfig, "")
@@ -239,7 +239,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if rs, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply replicaset %q failed: %v\n", rs.Name, err)
+				klog.V(4).Info("apply replicaset %q failed: %v\n", rs.Name, err)
 			}
 		case *storagev1.StorageClass:
 			handler, err := storageclass.New(ctx, kubeconfig)
@@ -247,7 +247,7 @@ func ApplyF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if sc, err := handler.Apply(k8sResource); err != nil {
-				log.Errorf("apply storageclass %q failed: %v\n", sc.Name, err)
+				klog.V(4).Info("apply storageclass %q failed: %v\n", sc.Name, err)
 			}
 		default:
 		}
--- delete_f.go
+++ delete_f.go
@@ -29,13 +29,13 @@ import (
 	"github.com/forbearing/k8s/serviceaccount"
 	"github.com/forbearing/k8s/statefulset"
 	"github.com/forbearing/k8s/storageclass"
-	log "github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	networking "k8s.io/api/networking/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	storagev1 "k8s.io/api/storage/v1"
+	"k8s.io/klog"
 )
 
 // DeleteF like "kubectl delete -f filename"
@@ -57,7 +57,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 		}
 		object, err := Decode(k8sResource)
 		if err != nil {
-			log.Error(err)
+			klog.V(4).Info(err)
 			continue
 		}
 		switch object.(type) {
@@ -67,7 +67,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.Service:
 			handler, err := service.New(ctx, kubeconfig, "")
@@ -75,7 +75,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.ConfigMap:
 			handler, err := configmap.New(ctx, kubeconfig, "")
@@ -83,7 +83,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.Secret:
 			handler, err := secret.New(ctx, kubeconfig, "")
@@ -91,7 +91,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.ServiceAccount:
 			handler, err := serviceaccount.New(ctx, kubeconfig, "")
@@ -99,7 +99,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.Pod:
 			handler, err := pod.New(ctx, kubeconfig, "")
@@ -107,7 +107,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.PersistentVolume:
 			handler, err := persistentvolume.New(ctx, kubeconfig)
@@ -115,7 +115,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.PersistentVolumeClaim:
 			handler, err := persistentvolumeclaim.New(ctx, kubeconfig, "")
@@ -123,7 +123,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *appsv1.Deployment:
 			handler, err := deployment.New(ctx, kubeconfig, "")
@@ -131,7 +131,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *appsv1.StatefulSet:
 			handler, err := statefulset.New(ctx, kubeconfig, "")
@@ -139,7 +139,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *appsv1.DaemonSet:
 			handler, err := daemonset.New(ctx, kubeconfig, "")
@@ -147,7 +147,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *networking.Ingress:
 			handler, err := ingress.New(ctx, kubeconfig, "")
@@ -155,7 +155,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *networking.IngressClass:
 			handler, err := ingressclass.New(ctx, kubeconfig)
@@ -163,7 +163,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *networking.NetworkPolicy:
 			handler, err := networkpolicy.New(ctx, kubeconfig, "")
@@ -171,7 +171,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *batchv1.Job:
 			handler, err := job.New(ctx, kubeconfig, "")
@@ -179,7 +179,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *batchv1.CronJob:
 			handler, err := cronjob.New(ctx, kubeconfig, "")
@@ -187,7 +187,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *rbacv1.Role:
 			handler, err := role.New(ctx, kubeconfig, "")
@@ -195,7 +195,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *rbacv1.RoleBinding:
 			handler, err := rolebinding.New(ctx, kubeconfig, "")
@@ -203,7 +203,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *rbacv1.ClusterRole:
 			handler, err := clusterrole.New(ctx, kubeconfig)
@@ -211,7 +211,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *rbacv1.ClusterRoleBinding:
 			handler, err := clusterrolebinding.New(ctx, kubeconfig)
@@ -219,7 +219,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *appsv1.ReplicaSet:
 			handler, err := replicaset.New(ctx, kubeconfig, "")
@@ -227,7 +227,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *corev1.ReplicationController:
 			handler, err := replicationcontroller.New(ctx, kubeconfig, "")
@@ -235,7 +235,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		case *storagev1.StorageClass:
 			handler, err := storageclass.New(ctx, kubeconfig)
@@ -243,7 +243,7 @@ func DeleteF(ctx context.Context, kubeconfig, filename string) error {
 				return err
 			}
 			if err := handler.Delete(k8sResource); err != nil {
-				log.Error(err)
+				klog.V(4).Info(err)
 			}
 		default:
 		}
