41a138cefef17a5a820f3b2c3d468d3c7dc6c3e8
hybfkuf <fredjoseph863@gmail.com>
2022-08-01 14:07:02 +0800

    Change bufio.ScanBytes -> bufio.ScanLines

--- pod/logs.go
+++ pod/logs.go
@@ -122,9 +122,9 @@ func (h *Handler) getLog(namespace, name string, logOptions *LogOptions) error {
 	defer readCloser.Close()
 
 	scanner := bufio.NewScanner(readCloser)
-	scanner.Split(bufio.ScanBytes)
+	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		fmt.Fprintf(logOptions.Writer, "%s", scanner.Text())
+		fmt.Fprintf(logOptions.Writer, "%s\n", scanner.Text())
 	}
 
 	//// if logOptions.Writer already closed, return nil.
