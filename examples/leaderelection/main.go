2a1576b19d3cbe608e1c3f041fec8fe0ca216884
hybfkuf <fredjoseph863@gmail.com>
2022-08-04 16:49:33 +0800

    Update leader election examples

--- examples/leaderelection/main.go
+++ examples/leaderelection/main.go
@@ -2,136 +2,63 @@ package main
 
 import (
 	"context"
-	"flag"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"github.com/google/uuid"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"github.com/forbearing/k8s"
+	"github.com/forbearing/k8s/leaderelection"
+	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/tools/leaderelection"
-	"k8s.io/client-go/tools/leaderelection/resourcelock"
-	"k8s.io/klog"
+)
+
+var (
+	leaderElectionID        = "my-test-id"
+	leaderElectionName      = "my-test-name"
+	leaderElectionNamespace = "test"
 )
 
 func main() {
-	var (
-		id                 string
-		kubeconfig         string
-		leaseLockName      string
-		leaseLockNamespace string
-	)
-
-	flag.StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
-	flag.StringVar(&id, "id", uuid.New().String(), "the holder identity name")
-	flag.StringVar(&leaseLockName, "lease-lock-name", "", "the lease lock resource name")
-	flag.StringVar(&leaseLockNamespace, "lease-lock-namespace", "", "the lease lock resource namespace")
-	flag.Parse()
-
-	if len(leaseLockName) == 0 {
-		klog.Fatal("unable to get lease lock resource name (missing lease-lock-name flag).")
-	}
-	if leaseLockNamespace == "" {
-		klog.Fatal("unable to get lease lock resource namespace (missing lease-lock-namespace flag).")
-	}
-
-	// leader election uses the Kubernetes API by writing to a
-	// lock object, which can be a LeaseLock object (preferred),
-	// a ConfigMap, or an Endpoints (deprecated) object.
-	// Conflicting writes are detected and each client handles those actions
-	// independently.
-	config, err := buildConfig(kubeconfig)
-	if err != nil {
-		klog.Fatal(err)
-	}
-	client := kubernetes.NewForConfigOrDie(config)
-
-	run := func(ctx context.Context) {
-		// complete your controller loop here
-		klog.Info("Controller loop...")
-		select {}
-	}
-
-	// use a Go context so we can tell the leaderelection code when we
-	// want to step down
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.TODO())
 	defer cancel()
-	// listen for interrupts or the Linux SIGTERM signal and cancel
-	// our context, which the leader election code will observe and
-	// step down
 	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-sigCh
-		klog.Info("Received termination, signaling shutdown")
+		log.Info("Received termination, signaling shutdown")
 		cancel()
 	}()
 
-	// we use the Lease lock type since edits to Leases are less common
-	// and fewer objects in the cluster watch "all Leases".
-	lock := &resourcelock.LeaseLock{
-		LeaseMeta: metav1.ObjectMeta{
-			Name:      leaseLockName,
-			Namespace: leaseLockNamespace,
-		},
-		Client: client.CoordinationV1(),
-		LockConfig: resourcelock.ResourceLockConfig{
-			Identity: id,
-		},
+	onStartedLeading := func() {
+		log.Info("Starting myserver")
+		select {}
 	}
-
-	// start the leader election code loop
-	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
-		Lock: lock,
-		// IMPORTANT: you MUST ensure that any code you have that
-		// is protected by the lease must terminate **before**
-		// you call cancel. Otherwise, you could have a background
-		// loop still running and another process could
-		// get elected before your background loop finished, violating
-		// the stated goal of the lease.
-		ReleaseOnCancel: true,
-		LeaseDuration:   60 * time.Second,
-		RenewDeadline:   15 * time.Second,
-		RetryPeriod:     5 * time.Second,
-		Callbacks: leaderelection.LeaderCallbacks{
-			OnStartedLeading: func(ctx context.Context) {
-				// we're notified when we start - this is where you would
-				// usually put your code
-				run(ctx)
-			},
-			OnStoppedLeading: func() {
-				// we can do cleanup here
-				klog.Infof("leader lost: %s", id)
-				os.Exit(0)
-			},
-			OnNewLeader: func(identity string) {
-				// we're notified when new leader elected
-				if identity == id {
-					// I just got the lock
-					return
-				}
-				klog.Infof("new leader elected: %s", identity)
-			},
-		},
-	})
-}
-
-func buildConfig(kubeconfig string) (*rest.Config, error) {
-	if kubeconfig != "" {
-		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			return nil, err
+	onStoppedLeading := func() {
+		// start cleanup
+		log.Infof("leader lost: %s", leaderElectionID)
+		os.Exit(0)
+	}
+	onNewLeader := func(identity string) {
+		// we're notified when new leader elected
+		if identity == leaderElectionID {
+			// I just got the lock
+			return
 		}
-		return cfg, nil
+		log.Infof("new leader elected: %s", identity)
 	}
 
-	cfg, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
+	options := leaderelection.Options{
+		LeaderElectionID:        leaderElectionID,
+		LeaderElectionName:      leaderElectionName,
+		LeaderElectionNamespace: leaderElectionNamespace,
+		LeaseDuration:           time.Second * 15,
+		RenewDeadline:           time.Second * 10,
+		RetryPeriod:             time.Second * 2,
+		OnStartedLeading:        onStartedLeading,
+		OnStoppedLeading:        onStoppedLeading,
+		OnNewLeader:             onNewLeader,
 	}
-	return cfg, nil
+
+	leaderelection.RunOrDie(ctx, k8s.RESTConfigOrDie(clientcmd.RecommendedHomeFile), options)
 }
