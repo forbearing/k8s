ce4df4536c4d77b517f912d242d0a6f3f5430d98
hybfkuf <fredjoseph863@gmail.com>
2022-08-04 17:17:29 +0800

    Delete interface Watcher, Lister.

--- types/interface.go
+++ types/interface.go
@@ -11,54 +11,31 @@ type Handler interface {
 	Applyer
 	Deleter
 	Geter
-	Lister
-	Watcher
 }
 
+// Creater
 type Creater interface {
-	//CreateFromRaw(raw map[string]interface{}) (interface{}, error)
-	//CreateFromBytes(data []byte) (interface{}, error)
-	//CreateFromFile(path string) (interface{}, error)
-	Create(name string) (interface{}, error)
+	Create(obj interface{}) (interface{}, error)
 }
 
+// Updater
 type Updater interface {
-	//UpdateFromRaw(raw map[string]interface{}) (interface{}, error)
-	//UpdateFromBytes(data []byte) (interface{}, error)
-	//UpdateFromFile(path string) (interface{}, error)
-	Update(name string) (interface{}, error)
+	Update(obj interface{}) (interface{}, error)
 }
 
+// Applyer
 type Applyer interface {
-	//ApplyFromRaw(raw map[string]interface{}) (interface{}, error)
-	//ApplyFromBytes(data []byte) (interface{}, error)
-	//ApplyFromFile(path string) (interface{}, error)
-	Apply(name string) (interface{}, error)
+	Apply(obj interface{}) (interface{}, error)
 }
+
+// Deleter
 type Deleter interface {
-	//DeleteByName(data []byte) error
-	//DeleteFromBytes(data []byte) error
-	//DeleteFromFile(path string) error
-	Delete(name string) error
+	Delete(obj interface{}) error
 }
 
+// Geter
 type Geter interface {
-	//GetByName(name string) (interface{}, error)
-	//GetFromBytes(name string) (interface{}, error)
-	//GetFromFile(path string) (interface{}, error)
-	Get(name string) (interface{}, error)
-}
-
-type Lister interface {
-	//ListByLabel(label string) (interface{}, error)
-	//ListAll() (interface{}, error)
-	List(label string) (interface{}, error)
-}
-
-type Watcher interface {
-	//WatchByName(name string, addFunc, modifyFunc, deleteFunc func(x interface{}), x interface{}) error
-	//WatchByLabel(label string, addFunc, modifyFunc, deleteFunc func(x interface{}), x interface{}) error
-	Watch(name string, addFunc, modifyFunc, deleteFunc func(x interface{}), x interface{}) error
+	Get(obj interface{}) (interface{}, error)
 }
 
 // Object is a Kubernetes object, allows functions to work indistinctly with
