33f354843c312e9c64f36e906a6f86fa16caf999
hybfkuf <fredjoseph863@gmail.com>
2022-08-09 19:33:38 +0800

    Add ListByField(), Update ListXXX returned value type: *appsv1.DeploymentList -> []*appsv1.Deployment

--- deployment/list.go
+++ deployment/list.go
@@ -3,27 +3,59 @@ package deployment
 import (
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 )
 
-// List list deployments by labels, alias to "ListByLabel".
-func (h *Handler) List(label string) (*appsv1.DeploymentList, error) {
-	return h.ListByLabel(label)
+// List list all deployments in the k8s cluster, it simply call `ListAll`.
+func (h *Handler) List(label string) ([]*appsv1.Deployment, error) {
+	return h.ListAll()
 }
 
 // ListByLabel list deployments by labels.
-func (h *Handler) ListByLabel(labels string) (*appsv1.DeploymentList, error) {
+// multiple labels separated by comma(",") eg: "name=myapp,role=devops"
+// There is an "And" relationship between multiple labels.
+func (h *Handler) ListByLabel(labels string) ([]*appsv1.Deployment, error) {
 	//h.Options.ListOptions.LabelSelector = labelSelector
 	listOptions := h.Options.ListOptions.DeepCopy()
 	listOptions.LabelSelector = labels
-	return h.clientset.AppsV1().Deployments(h.namespace).List(h.ctx, *listOptions)
+	deployList, err := h.clientset.AppsV1().Deployments(h.namespace).List(h.ctx, *listOptions)
+	if err != nil {
+		return nil, err
+	}
+	return extractList(deployList), nil
+}
+
+// ListByField list deployments by field, work like `kubectl get xxx --field-selector=xxx`
+func (h *Handler) ListByField(field string) ([]*appsv1.Deployment, error) {
+	fieldSelector, err := fields.ParseSelector(field)
+	if err != nil {
+		return nil, err
+	}
+	listOptions := h.Options.ListOptions.DeepCopy()
+	listOptions.FieldSelector = fieldSelector.String()
+
+	deployList, err := h.clientset.AppsV1().Deployments(h.namespace).List(h.ctx, *listOptions)
+	if err != nil {
+		return nil, err
+	}
+	return extractList(deployList), nil
 }
 
 // ListByNamespace list all deployments in the specified namespace.
-func (h *Handler) ListByNamespace(namespace string) (*appsv1.DeploymentList, error) {
+func (h *Handler) ListByNamespace(namespace string) ([]*appsv1.Deployment, error) {
 	return h.WithNamespace(namespace).ListByLabel("")
 }
 
 // ListAll list all deployments in the k8s cluster.
-func (h *Handler) ListAll() (*appsv1.DeploymentList, error) {
+func (h *Handler) ListAll() ([]*appsv1.Deployment, error) {
 	return h.WithNamespace(metav1.NamespaceAll).ListByLabel("")
+}
+
+// extractList
+func extractList(deployList *appsv1.DeploymentList) []*appsv1.Deployment {
+	var objList []*appsv1.Deployment
+	for i := range deployList.Items {
+		objList = append(objList, &deployList.Items[i])
+	}
+	return objList
 }
