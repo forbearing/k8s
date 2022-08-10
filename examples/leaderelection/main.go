111c1306846500b7c43b18d36947c448663a5d71
hybfkuf <fredjoseph863@gmail.com>
2022-08-10 17:21:50 +0800

    Update leader election examples

--- examples/leaderelection/main.go
+++ examples/leaderelection/main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/forbearing/k8s"
-	"github.com/forbearing/k8s/tools/leaderelection"
+	"github.com/forbearing/k8s/utils/leaderelection"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/tools/clientcmd"
 )
