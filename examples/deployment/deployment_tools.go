0ad35f7e901651d678cf0b03b74caab3ba0792c5
hybfkuf <fredjoseph863@gmail.com>
2022-08-18 11:00:45 +0800

    Update deployment examples

--- examples/deployment/deployment_tools.go
+++ examples/deployment/deployment_tools.go
@@ -38,7 +38,7 @@ func Deployment_Tools() {
 		checkErr("GetRS", printRS(rsList), err)
 		// GetPods get all pods that created by the deployment.
 		podList, err := handler.GetPods(name2)
-		checkErr("GetRS", printPods(podList), err)
+		checkErr("GetPods", printPods(podList), err)
 		// GetPV get all persistentvolume that attached by the deployment.
 		pvList, err := handler.GetPV(name2)
 		checkErr("GetPV", pvList, err)
@@ -60,7 +60,7 @@ func Deployment_Tools() {
 		checkErr("GetRS", printRS(rsList), err)
 		// GetPods get all pods that created by the deployment.
 		podList, err := handler.GetPods(deploy)
-		checkErr("GetRS", printPods(podList), err)
+		checkErr("GetPods", printPods(podList), err)
 		// GetPV get all persistentvolume that attached by the deployment.
 		pvList, err := handler.GetPV(deploy)
 		checkErr("GetPV", pvList, err)
@@ -81,27 +81,43 @@ func Deployment_Tools() {
 	//2022/07/12 09:22:16 true
 	//2022/07/12 09:22:16 ===== Get Deployment By Name
 	//2022/07/12 09:22:16 GetRS success: [nginx-deploy-79979d95dd]
-	//2022/07/12 09:22:16 GetRS success: [nginx-deploy-79979d95dd-4lm8h nginx-deploy-79979d95dd-5l9rk nginx-deploy-79979d95dd-scjw9]
+	//2022/07/12 09:22:16 GetPods success: [nginx-deploy-79979d95dd-4lm8h nginx-deploy-79979d95dd-5l9rk nginx-deploy-79979d95dd-scjw9]
 	//2022/07/12 09:22:16 GetPV success: [pvc-93ebe9a0-c464-439b-a252-51afb4d87069 pvc-c048ccf9-4d0c-4312-bb36-15e4fa7a1746 pvc-dc16fea0-f511-42d7-b78e-6fcac96fcc9b]
 	//2022/07/12 09:22:16 GetPVC success: [deploy-k8s-tools-data deploy-nginx-data deploy-nginx-html]
 	//2022/07/12 09:22:16 ===== Get Deployment By Name Cost Time: 82.467272ms
 	//2022/07/12 09:22:16
 	//2022/07/12 09:22:16 ===== Get Deployment By Object
 	//2022/07/12 09:22:16 GetRS success: [nginx-deploy-79979d95dd]
-	//2022/07/12 09:22:16 GetRS success: [nginx-deploy-79979d95dd-4lm8h nginx-deploy-79979d95dd-5l9rk nginx-deploy-79979d95dd-scjw9]
+	//2022/07/12 09:22:16 GetPods success: [nginx-deploy-79979d95dd-4lm8h nginx-deploy-79979d95dd-5l9rk nginx-deploy-79979d95dd-scjw9]
 	//2022/07/12 09:22:17 GetPV success: [pvc-93ebe9a0-c464-439b-a252-51afb4d87069 pvc-c048ccf9-4d0c-4312-bb36-15e4fa7a1746 pvc-dc16fea0-f511-42d7-b78e-6fcac96fcc9b]
 	//2022/07/12 09:22:17 GetPVC success: [deploy-k8s-tools-data deploy-nginx-data deploy-nginx-html]
 	//2022/07/12 09:22:17 ===== Get Deployment By Object Cost Time: 134.639944ms
+
+	//2022/08/18 10:38:25 true
+	//2022/08/18 10:38:25 true
+	//2022/08/18 10:38:25 ===== Get Deployment By Name
+	//2022/08/18 10:38:25 GetRS success: [nginx-deploy-547d7f976b nginx-deploy-6cf4f6cf66 nginx-deploy-6fd74c44cb]
+	//2022/08/18 10:38:25 GetPods success: [nginx-deploy-547d7f976b-fgl7w nginx-deploy-6fd74c44cb-9pvn5 nginx-deploy-6fd74c44cb-fxfnr nginx-deploy-6fd74c44cb-h8p52]
+	//2022/08/18 10:38:25 GetPV success: [pvc-ff43298b-c07b-400f-ad64-045133d6261d pvc-29afe86b-9cbc-4c39-921e-1ea1bcc467b4 pvc-8167afa1-9493-459a-8352-fb7496346aa9]
+	//2022/08/18 10:38:25 GetPVC success: [deploy-k8s-tools-data deploy-nginx-data deploy-nginx-html]
+	//2022/08/18 10:38:25 ===== Get Deployment By Name Cost Time: 49.89824ms
+	//2022/08/18 10:38:25
+	//2022/08/18 10:38:25 ===== Get Deployment By Object
+	//2022/08/18 10:38:25 GetRS success: [nginx-deploy-547d7f976b nginx-deploy-6cf4f6cf66 nginx-deploy-6fd74c44cb]
+	//2022/08/18 10:38:26 GetPods success: [nginx-deploy-547d7f976b-fgl7w nginx-deploy-6fd74c44cb-9pvn5 nginx-deploy-6fd74c44cb-fxfnr nginx-deploy-6fd74c44cb-h8p52]
+	//2022/08/18 10:38:26 GetPV success: [pvc-ff43298b-c07b-400f-ad64-045133d6261d pvc-29afe86b-9cbc-4c39-921e-1ea1bcc467b4 pvc-8167afa1-9493-459a-8352-fb7496346aa9]
+	//2022/08/18 10:38:26 GetPVC success: [deploy-k8s-tools-data deploy-nginx-data deploy-nginx-html]
+	//2022/08/18 10:38:26 ===== Get Deployment By Object Cost Time: 354.565842ms
 }
 
-func printPods(podList []corev1.Pod) []string {
+func printPods(podList []*corev1.Pod) []string {
 	var pl []string
 	for _, pod := range podList {
 		pl = append(pl, pod.Name)
 	}
 	return pl
 }
-func printRS(rsList []appsv1.ReplicaSet) []string {
+func printRS(rsList []*appsv1.ReplicaSet) []string {
 	var rl []string
 	for _, rs := range rsList {
 		rl = append(rl, rs.Name)
