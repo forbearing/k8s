796a1ee086d8657fc2570512312d62b7d8492111
hybfkuf <fredjoseph863@gmail.com>
2022-08-23 23:54:15 +0800

    Add comma in ErrInvalidToolsType

--- deployment/types.go
+++ deployment/types.go
@@ -3,7 +3,7 @@ package deployment
 import "fmt"
 
 var (
-	ErrInvalidToolsType  = fmt.Errorf("type must be string *appsv1.Deployment, or appsv1.Deployment")
+	ErrInvalidToolsType  = fmt.Errorf("type must be string, *appsv1.Deployment, or appsv1.Deployment")
 	ErrInvalidCreateType = fmt.Errorf("type must be string, []byte, *appsv1.Deployment, appsv1.Deployment, runtime.Object, *unstructured.Unstructured, unstructured.Unstructured or map[string]interface{}")
 	ErrInvalidUpdateType = ErrInvalidCreateType
 	ErrInvalidApplyType  = ErrInvalidCreateType
