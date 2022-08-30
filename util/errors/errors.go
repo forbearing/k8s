b8746e39b3ab78ae920f47f94c8308c297277fbb
hybfkuf <fredjoseph863@gmail.com>
2022-08-30 11:49:26 +0800

    Add IgnoreDeadlineExceeded() to ignore context timeout error

--- util/errors/errors.go
+++ util/errors/errors.go
@@ -1,6 +1,11 @@
 package errors
 
-import apierrors "k8s.io/apimachinery/pkg/api/errors"
+import (
+	"context"
+	"errors"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+)
 
 // IgnoreNotFound returns nil on NotFound errors.
 // All other values that are not NotFound errors or nil are returned unmodified.
@@ -15,6 +20,15 @@ func IgnoreNotFound(err error) error {
 // All other values that are not AlreadyExists errors or nil are returned unmodified.
 func IgnoreAlreadyExists(err error) error {
 	if apierrors.IsAlreadyExists(err) {
+		return nil
+	}
+	return err
+}
+
+// IgnoreDeadlineExceeded returns nil on context.DeadlineExceeded errors.
+// All other values that are not context.DeadlineExceeded or nil are returned unmodified.
+func IgnoreDeadlineExceeded(err error) error {
+	if errors.Is(err, context.DeadlineExceeded) {
 		return nil
 	}
 	return err
