3b63def2fab76196335228b97c1afdeba468aee3
hybfkuf <fredjoseph863@gmail.com>
2022-08-30 11:51:08 +0800

    Add NewSignalContext() and NewSignalChannel() to new ctx and chan for signal

--- util/signals/signal.go
+++ util/signals/signal.go
@@ -15,11 +15,12 @@ var (
 // SetupSignalContext registers for SIGTERM and SIGINT. A context is returned
 // which is canceled on one of these signals. If a second signal is caught, the program
 // is terminated with exit code 1.
+//
+// It will panic when call SetupSignalContext twice.
 func SetupSignalContext() context.Context {
 	close(onlyOneSignalHandler) // panics when called twice
 
 	ctx, cancel := context.WithCancel(context.Background())
-
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, shutdownSignals...)
 	go func() {
@@ -35,6 +36,8 @@ func SetupSignalContext() context.Context {
 // SetupSignalChannel registered for SIGTERM and SIGINT. A stop channel is returned
 // which is closed on one of these signals. If a second signal is caught, the program
 // is terminated with exit code 1.
+//
+// It will panic when call SetupSignalChannel twice.
 func SetupSignalChannel() (stopCh <-chan struct{}) {
 	close(onlyOneSignalHandler) // panics when called twice
 
@@ -46,6 +49,44 @@ func SetupSignalChannel() (stopCh <-chan struct{}) {
 		close(stop)
 		<-c
 		os.Exit(1) // second signal. Exit directly.
+	}()
+
+	return stop
+}
+
+// NewSignalContext registers for SIGTERM and SIGINT. A context is returned
+// which is canceled on one of these signals. If a second signal is caught, the program
+// is terminated with exit code 1.
+//
+// You can call NewSignalContext() multiple time.
+func NewSignalContext() context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, shutdownSignals...)
+	go func() {
+		<-c
+		cancel()
+		//<-c
+		//os.Exit(1) // second signal. Exit directly.
+	}()
+
+	return ctx
+}
+
+// NewSignalChannel registered for SIGTERM and SIGINT. A stop channel is returned
+// which is closed on one of these signals. If a second signal is caught, the program
+// is terminated with exit code 1.
+//
+// You can call NewSignalChannel() multiple time.
+func NewSignalChannel() (stopCh <-chan struct{}) {
+	stop := make(chan struct{})
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, shutdownSignals...)
+	go func() {
+		<-c
+		close(stop)
+		//<-c
+		//os.Exit(1) // second signal. Exit directly.
 	}()
 
 	return stop
