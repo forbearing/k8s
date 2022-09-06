016dbe9b3ce0c0274a2ca4175f894195b2ac25eb
hybfkuf <fredjoseph863@gmail.com>
2022-09-06 21:02:45 +0800

    Update Update() document

--- dynamic/update.go
+++ dynamic/update.go
@@ -16,9 +16,10 @@ import (
 // runtime.Object, *unstructured.Unstructured, unstructured.Unstructured
 // or map[string]interface{}.
 //
-// It's not necessary to explicitly specify the GVK or GVR, Update() will find
-// the GVK and GVR by RESTMapper and update the k8s resource that defined in
-// yaml file, json file, bytes data, map[string]interface{} or runtime.Object.
+// It's not necessary to explicitly specify the GVK or GVR by calling WithGVK(),
+// Update() will find the GVK and GVR by RESTMapper and update the k8s resource
+// that defined in yaml file, json file, bytes data, map[string]interface{}
+// or runtime.Object.
 func (h *Handler) Update(obj interface{}) (*unstructured.Unstructured, error) {
 	switch val := obj.(type) {
 	case string:
