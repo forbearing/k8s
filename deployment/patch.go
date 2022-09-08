49de9e3fdca959f115afd089de9f2c57ac8aea62
hybfkuf <fredjoseph863@gmail.com>
2022-09-08 22:49:59 +0800

    Update nothing

--- deployment/patch.go
+++ deployment/patch.go
@@ -86,24 +86,6 @@ func (h *Handler) StrategicMergePatch(original, modified *appsv1.Deployment) (*a
 		types.StrategicMergePatchType, patchData, h.Options.PatchOptions)
 }
 
-// JsonPath use JSON Patch to patch deployment.
-//
-// For a comparison of JSON patch and JSON merge patch, see:
-//     https://erosb.github.io/post/json-patch-vs-merge-patch/
-// For further more Json Merge Patch see:
-//     https://kubernetes.io/docs/tasks/manage-kubernetes-objects/update-api-object-kubectl-patch/#before-you-begin
-//     https://tools.ietf.org/html/rfc7386
-func (h *Handler) JsonPath(deploy *appsv1.Deployment, patchData []byte) (*appsv1.Deployment, error) {
-	var namespace string
-	if len(deploy.Namespace) != 0 {
-		namespace = deploy.Namespace
-	} else {
-		namespace = h.namespace
-	}
-	return h.clientset.AppsV1().Deployments(namespace).Patch(h.ctx,
-		deploy.Name, types.JSONPatchType, patchData, h.Options.PatchOptions)
-}
-
 // MergePatch use the JSON Merge Patch to patch deployment.
 // A JSON merge patch is different from strategic merge patch, With a JSON merge patch,
 // If you want to update a list, you have to specify the entire new list.
@@ -144,4 +126,22 @@ func (h *Handler) MergePatch(original, modified *appsv1.Deployment) (*appsv1.Dep
 
 	return h.clientset.AppsV1().Deployments(namespace).Patch(h.ctx, original.Name,
 		types.MergePatchType, patchData, h.Options.PatchOptions)
+}
+
+// JsonPath use JSON Patch to patch deployment.
+//
+// For a comparison of JSON patch and JSON merge patch, see:
+//     https://erosb.github.io/post/json-patch-vs-merge-patch/
+// For further more Json Merge Patch see:
+//     https://kubernetes.io/docs/tasks/manage-kubernetes-objects/update-api-object-kubectl-patch/#before-you-begin
+//     https://tools.ietf.org/html/rfc7386
+func (h *Handler) JsonPath(deploy *appsv1.Deployment, patchData []byte) (*appsv1.Deployment, error) {
+	var namespace string
+	if len(deploy.Namespace) != 0 {
+		namespace = deploy.Namespace
+	} else {
+		namespace = h.namespace
+	}
+	return h.clientset.AppsV1().Deployments(namespace).Patch(h.ctx,
+		deploy.Name, types.JSONPatchType, patchData, h.Options.PatchOptions)
 }
