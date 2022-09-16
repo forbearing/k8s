9f7c242b5b4f2063a77273571350ef59ebc0b20a
hybfkuf <fredjoseph863@gmail.com>
2022-09-16 09:54:17 +0800

    Add variables: ErrInvalidCreateType, ErrInvalidUpdateType, ErrInvalidApplyType, ErrInvaliDeleteType, ErrInvalidGetType, ErrInvalidPatchType

--- dynamic/types.go
+++ dynamic/types.go
@@ -3,8 +3,13 @@ package dynamic
 import "errors"
 
 var (
-	ErrGroupEmpty    = errors.New("group must not be empty")
-	ErrVersionEmpty  = errors.New("version must not be empty")
-	ErrResourceEmpty = errors.New("resource must not be empty")
-	ErrInvalidType   = errors.New("type must be string, []byte, runtime.Object, *unstructured.Unstructured, unstructured.Unstructured or map[string]interface{}")
+	ErrGroupEmpty        = errors.New("group must not be empty")
+	ErrVersionEmpty      = errors.New("version must not be empty")
+	ErrResourceEmpty     = errors.New("resource must not be empty")
+	ErrInvalidCreateType = errors.New("type must be string, []byte, runtime.Object, *unstructured.Unstructured, unstructured.Unstructured or map[string]interface{}")
+	ErrInvalidUpdateType = ErrInvalidCreateType
+	ErrInvalidApplyType  = ErrInvalidCreateType
+	ErrInvalidDeleteType = ErrInvalidCreateType
+	ErrInvalidGetType    = ErrInvalidCreateType
+	ErrInvalidPatchType  = errors.New("patch type must be string, []byte, runtime.Object, *unstructured.Unstructured, unstructured.Unstructured or map[string]interface{}")
 )
